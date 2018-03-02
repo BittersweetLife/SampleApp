0aa11726a2a93dbe413ca60bf3e16595697daeba
ABittersweetLife <deva_kdr@yahoo.com>
2018-03-02 23:22:34 +0530

    all back to org

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ var server = http.createServer(function(request, response) {
 // });
 
 var port = process.env.PORT || 1337;
-server.listen(8080);
+server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
