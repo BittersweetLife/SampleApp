afd3218cc989b738279c43c393fa4d9a992d4479
ABittersweetLife <deva_kdr@yahoo.com>
2018-03-02 22:21:20 +0530

    changes 2

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 var http = require('http');
-var fs = require('fs');
+const express = require('express')
+const app = express()
 
-var handler = function(req,res){
-    fs.readFile(__dirname + "/client.html",function(err,data){
-        if(err){
-            res.writeHead(500);
-        }else{
-            res.writeHead(200);
-            res.end(data);
-        }
-    });
-};
+app.get('/', (req, res) => res.send('Hello World!'))
 
-var app = http.createServer(handler)
-app.listen(8080);
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+// var fs = require('fs');
+
+// var handler = function(req,res){
+//     fs.readFile(__dirname + "/client.html",function(err,data){
+//         if(err){
+//             res.writeHead(500);
+//         }else{
+//             res.writeHead(200);
+//             res.end(data);
+//         }
+//     });
+// };
+
+// var app = http.createServer(handler)
+// app.listen(8080);
 
 console.log("Server running!!!")
