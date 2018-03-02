41b8b99ea6114d1a8a2b7c858f2c5b3417562de9
ABittersweetLife <deva_kdr@yahoo.com>
2018-03-02 22:37:05 +0530

    changes 4

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-var http = require('http');
+var http = require("http");
+var fs = require("fs");
 
 var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    // response.writeHead(200, {"Content-Type": "text/plain"});
+    // response.end("Hello World!");
+
+    fs.readFile(__dirname + "/client.html",function(err,data){
+        if(err){
+            response.writeHead(500);
+        }else{
+            //response.writeHead(200, {"Content-Type": "text/plain"});
+            response.writeHead(200);            
+            response.end(data);
+        }
+    });
 
 });
 
